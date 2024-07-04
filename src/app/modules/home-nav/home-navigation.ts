ddb41d371bf2ebbead03b342198062d17f982be9
Anil Maharjan <anilmaharjan@mentorfriends.com>
2024-07-04 13:06:45 +0545

    update: navigation active according on click/change

--- src/app/modules/home-nav/home-navigation.ts
+++ src/app/modules/home-nav/home-navigation.ts
@@ -2,9 +2,9 @@ import initNavigation from "./home-navigation.service";
 
 export default `
   <nav class="relative px-4 py-4 flex justify-between items-center bg-white">
-    <a class="text-3xl font-bold leading-none" href="#">
+    <router-link href="/" class="cursor-pointer hover:opacity-85">
       <img src="./images/humanizing-data.png" alt="humanizing data logo" class="w-16 md:w-32 lg:w-48">
-    </a>
+    </router-link>
     <div class="lg:hidden">
       <button class="navbar-burger flex items-center text-blue-600 p-3">
         <svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -15,13 +15,6 @@ export default `
     </div>
     <div class="flex justify-end">
       <ul class="hidden lg:flex lg:mx-auto lg:flex lg:items-center lg:mr-8 justify-end lg:w-auto lg:space-x-6">
-        <!-- <li><a class="text-sm text-gray-400 hover:text-gray-500" href="#">Home</a></li>
-            <li><a class="text-sm text-green-600 font-bold" href="#">About</a></li>
-            <li><a class="text-sm text-gray-400 hover:text-gray-500" href="#">Services</a></li>
-            <li><a class="text-sm text-gray-400 hover:text-gray-500" href="#">Blog</a></li>
-            <li><a class="text-sm text-gray-400 hover:text-gray-500" href="#">Contact</a></li>
-            <li><a class="text-sm text-gray-400 hover:text-gray-500" href="#">Career</a></li> -->
-
         <router-link href="/" class="cursor-pointer text-sm text-green-600 font-bold">Home</router-link>
         <router-link href="/" class="cursor-pointer text-sm text-gray-400 hover:text-gray-500">About</router-link>
         <router-link href="/" class="cursor-pointer text-sm text-gray-400 hover:text-gray-500">Services</router-link>
@@ -30,8 +23,6 @@ export default `
         <router-link href="/dashboard"
           class="cursor-pointer text-sm text-gray-400 hover:text-gray-500">Career</router-link>
       </ul>
-      <!-- <a class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-green-50 hover:bg-green-100 hover:text-green-900 text-sm text-green-900 font-bold rounded-xl transition duration-200" href="#">Sign In</a>
-          <a class="hidden lg:inline-block py-2 px-6 bg-green-500 hover:bg-green-600 hover:text-white text-sm text-white font-bold rounded-xl transition duration-200" href="#">Sign up</a> -->
 
       <router-link href="/login"
         class="block px-4 py-3 mb-2 mr-2 leading-loose text-xs text-center text-white font-semibold bg-green-600 hover:bg-green-700 rounded-xl cursor-pointer">Login</router-link>
--- src/app/modules/top-nav/top-navigation.service.ts
+++ src/app/modules/top-nav/top-navigation.service.ts
@@ -5,3 +5,20 @@ export async function logout() {
   location.reload()
   updateContent("/");
 }
+
+export async function initTopNavigation() {
+  console.log('INIT NAVIGATION')
+  const nav = document.getElementById('top-nav')
+  const navLinks: any = nav?.querySelectorAll('router-link')
+
+  navLinks?.forEach((navlink: any) => {
+    const routeName = navlink.getAttribute("href");
+    if (location.pathname === routeName) {
+      navlink.classList.add("active");
+    }
+
+    navlink?.addEventListener('click', () => {
+      initTopNavigation()
+    })
+  })
+}
