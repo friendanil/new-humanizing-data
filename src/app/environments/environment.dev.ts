51a5ce92edc6813ab94c4112ad04a2598511d4cf
nischalbhandari12 <nischal.bhandari@mentorfriends.com>
2024-08-09 13:10:57 +0545

    added changes

--- src/app/environments/environment.dev.ts
+++ src/app/environments/environment.dev.ts
@@ -1,9 +1,9 @@
 export const environment = {
   // DEVELOPMENT
-  baseURL: 'http://localhost:5000/api/v1',
-  // baseURL: 'https://theta.boomconcole.com/api/v1',
-  // baseNodeUrl: 'https://theta.boomconcole.com',
-  baseNodeUrl: 'http://localhost:5000',
+  //baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://theta.boomconcole.com/api/v1',
+  baseNodeUrl: 'https://theta.boomconcole.com',
+  //baseNodeUrl: 'http://localhost:5000',
   boomURL: 'https://theta.boomconcole.com',
   aiURL: 'https://www.devai.freeschema.com/api'
 
